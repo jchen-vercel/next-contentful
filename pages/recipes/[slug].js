85bf5a26f4330b47081591e30d9435c4aa803644
jchen-vercel <jason.chen@vercel.com>
2022-08-02 14:24:25 -0400

    implement skeleton fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -19,7 +20,7 @@ export const getStaticPaths = async () => {
   });
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -37,7 +38,7 @@ export async function getStaticProps({ params }) {
   };
 }
 export default function RecipeDetails({ recipe }) {
-  console.log({ recipe });
+  if (!recipe) return <Skeleton />;
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
